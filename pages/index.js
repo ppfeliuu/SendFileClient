452a5b543c2493e8b6c427244edec6b6efa519ec
Pedro Feliu <pedro@medanosol.es>
2020-09-11 13:48:42 +0100

    11092020

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import React from "react";
+import Layout from "../components/Layout";
 
-export default function Home() {
-  return <div className={styles.container}>Hi</div>;
-}
+const Index = () => {
+  return (
+    <Layout>
+      <h1>Index</h1>
+    </Layout>
+  );
+};
+
+export default Index;
