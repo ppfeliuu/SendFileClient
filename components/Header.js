02d4ae8ce0fec40533ed65481a13bfd48a06c41d
ppfeliuu <ppfeliuu@gmail.com>
2020-09-13 19:31:58 +0100

    13092020

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import Link from 'next/link'
 const Header = () => {
   return (
     <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-      <img className="w-64 mb-8 md:mb-0" src="logo.svg" />
+      <Link href="/">
+        <img className="w-64 mb-8 md:mb-0" src="logo.svg" />
+      </Link>
+      <div>
+        <Link href="/login">
+          <a className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2">Iniciar Sesión</a>
+        </Link>
+
+        <Link href="/register">
+          <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">Registrar</a>
+        </Link>
+      </div>
     </header>
   );
 };
