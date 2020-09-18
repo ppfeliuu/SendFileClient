01dbd5be9cc10aebf4dc3c07212859d8a1ef0970
ppfeliuu <ppfeliuu@gmail.com>
2020-09-18 15:31:15 +0100

    18092020

--- context/auth/authState.js
+++ context/auth/authState.js
@@ -1,11 +1,24 @@
 import React, { useReducer } from 'react'
 import authContext from './authContext'
+import authReducer from './authReducer';
 
 
 
 const AuthState = ({ children }) => {
+
+    //State inicial
+    const initialState = {
+        token: '',
+        autenticado: null,
+        usuario: null,
+        mensaje: null
+    }
+
+    //reducer
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
     return (
-        <authContext.Provider value={{}}>{children}
+        <authContext.Provider value={{ token: state.token, autenticado: state.autenticado, usuario: state.usuario, mensaje: state.mensaje }}>{children}
 
         </authContext.Provider>
     )
