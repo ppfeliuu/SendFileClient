9dbd1bb3a8dac4bede353178a94aa073174fbf3d
Pedro Feliu <pedro@medanosol.es>
2020-10-29 12:03:50 +0000

    29102020

---
+++ pages/enlaces/[enlaces].js
@@ -0,0 +1,14 @@
+import Layout from "../../components/Layout";
+import clienteAxios from "../../config/axios";
+
+export async function getStaticProps() {}
+
+export async function getStaticPath() {}
+
+export default () => {
+  return (
+    <Layout>
+      <h1></h1>
+    </Layout>
+  );
+};
